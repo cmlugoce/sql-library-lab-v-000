4abe34255a5db4c2c36840b551eab7f5b3f57092
Cristina Lugo <cmlugoce@syr.edu>
2018-07-05 22:36:25 +0000

    automatically backed up by learn

--- lib/schema.sql
+++ lib/schema.sql
@@ -1,8 +1,8 @@
 CREATE TABLE series (
-  id INTEGER PRIMARY KEY,
-  title TEXT,
-  sub-genre_id TEXT,
-  author_id TEXT
+    id INTEGER PRIMARY KEY,
+    title TEXT,
+    author_id INTEGER,
+    subgenre_id INTEGER
 );
 
 CREATE TABLE sub-genre (id INTEGER PRIMARY KEY, name TEXT);
