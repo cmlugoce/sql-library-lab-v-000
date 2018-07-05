9fe5a570af1983c0408721b4f92852e0653b0477
Cristina Lugo <cmlugoce@syr.edu>
2018-07-05 22:06:09 +0000

    automatically backed up by learn

---
+++ lib/schema.sql
@@ -0,0 +1,12 @@
+CREATE TABLE series (
+  id INTEGER PRIMARY KEY,
+  title TEXT,
+  sub-genre_id TEXT,
+  author_id TEXT
+);
+
+CREATE TABLE sub-genre (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
+CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, series_id INTEGER, author_id INTEGER);
+CREATE TABLE chracter_books (id INTEGER PRIMARY KEY, book_id INTEGER, character_id INTEGER );
