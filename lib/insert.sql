fcb598bdd96b7066c7c60031efc40fbd6a61c38b
Cristina Lugo <cmlugoce@syr.edu>
2018-07-05 22:26:38 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,7 +8,7 @@ INSERT INTO books (id, title, year, series_id) VALUES (1, "Game of Thrones", 199
 
 INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (1, "Lady", "Woof Woof", "direwolf", 1, 1), (2, "Tyrion Lannister", "A Lannister always pays his debts", "human", 1, 1), (3, "Daenerys Targaryen", "If I look back I am lost", "human", 1, 1), (4, "Eddard Stark", "Winter is coming", "human", 1, 1);
 
-INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Harry", "I'm going to keep going until I succeed — or die.", "wizard", 2, 2), (6, "Hermione", "motto two", "human/witch", 2, 2), (7, "Ron", "When in doubt, go to the library.", "wizard", 2, 2), (8, "Dumbledore", "We are only as strong as we are united, as weak as we are divided", "wizard", 2, 2);
+INSERT INTO characters (id, name, motto, species, author_id, series_id) VALUES (5, "Harry", "I'm going to keep going until I succeed — or die.", "wizard", 2, 2), (6, "Hermione", "Just because you've got the emotional range of a teaspoon doesn't mean we all have.", "human/witch", 2, 2), (7, "Ron", "When in doubt, go to the library.", "wizard", 2, 2), (8, "Dumbledore", "We are only as strong as we are united, as weak as we are divided", "wizard", 2, 2);
 
 INSERT INTO character_books (id, book_id, character_id) VALUES (1, 1, 1), (2, 1, 2), (3, 2, 2), (4, 3, 2), (5, 1, 3), (6, 2, 3), (7, 3, 3), (8, 1, 4);
 
