b2ac69d08e97078cf4e76cfc5789bdf654e4cd82
Cristina Lugo <cmlugoce@syr.edu>
2018-07-05 22:00:47 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -8,4 +8,5 @@ CREATE TABLE series (
 CREATE TABLE sub-genre (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE authors (id INTEGER PRIMARY KEY, name TEXT);
 CREATE TABLE books (id INTEGER PRIMARY KEY, title TEXT, year INTEGER, series_id INTEGER);
-CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, series_id INTEGER, author_id INTEGER)
+CREATE TABLE characters (id INTEGER PRIMARY KEY, name TEXT, motto TEXT, species TEXT, series_id INTEGER, author_id INTEGER)
+CREATE TABLE 
